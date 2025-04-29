74b05f103f9a70bdc88c999b272049e5de29e793
shrsyc <shrsyc@gmail.com>
2025-04-29 14:22:07 +0530

    week1_skeletoned

---
+++ project-week1/src/main/java/com/example/shrsyc/week1/project_week1/ProjectWeek1Application.java
@@ -0,0 +1,13 @@
+package com.example.shrsyc.week1.project_week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWeek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWeek1Application.class, args);
+	}
+
+}
---
+++ project-week1/src/test/java/com/example/shrsyc/week1/project_week1/ProjectWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shrsyc.week1.project_week1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWeek1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
